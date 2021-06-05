4bbc9f77f74523b9f4f1d311ba30f1ea5311ede8
NataliaMGonzalez <nataliagzzh@gmail.com>
2021-06-05 17:31:28 -0500

    can now edit posts

--- routes/routeindex.js
+++ routes/routeindex.js
@@ -3,10 +3,12 @@ const express = require('express');
 const router = express.Router();
 const BlogPost = require('../model/BlogPost');
 
-
-
-
 const User = require("../model/User");
+
+router.get('/all-posts', async function(req,res){
+  var blogPosts =  await BlogPost.find();
+  res.render('../views/partials/allPosts.ejs', {blogPosts});
+});
 
 router.get('/',async function(req,res){
     //console.log("User id: " + req.userId);
@@ -16,13 +18,27 @@ router.get('/',async function(req,res){
 router.get('/newPost', async function(req,res){
   res.render('newPost');
 });
-
+// NEW POST
 // 3 -> Ir a newPost.ejs y dar de alta action y method
 router.post('/newPost', async (req,res) =>{
     var blogPost = new BlogPost(req.body);
     await blogPost.save();
     res.redirect('/');
   });
+
+// EDIT POST
+// 5 -> Ir a edit.ejs y dar de alta action y method
+router.get('/edit/:id', async (req,res) =>{
+  var blogPost = await BlogPost.findById(req.params.id);
+  res.render('../views/editPost', {blogPost});
+});
+
+// 7
+router.post('/edit/:id',   async(req,res) =>{
+  var id = req.params.id;
+  await BlogPost.updateOne({_id: id},req.body );
+  res.redirect('/');
+});
 
 
 module.exports = router;
